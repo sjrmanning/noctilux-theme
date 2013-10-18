5bd1b844ae4220fd6f873b9d441daa9e1d0c27e6
Steve Purcell <steve@sanityinc.com>
2013-10-18 11:01:39 +0100

    Add header and footer lines for package.el compatibility
    This will hopefully allow MELPA to determine the description when building packages of this theme.

--- noctilux-theme.el
+++ noctilux-theme.el
@@ -1,5 +1,9 @@
+;;; noctilux-theme.el --- Dark theme inspired by LightTable
+
 (require 'noctilux-definitions
          (locate-file "noctilux-definitions.el" custom-theme-load-path
                       '("c" "")))
 
 (create-noctilux-theme)
+
+;;; noctilux-theme.el ends here
